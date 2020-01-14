359d6bd222bc9395f7a7c699fc5974e3753b1ded
purdyjen <purdyjen@me.com>
2020-01-14 12:57:21 -0600

    decent progress

---
+++ assets/js/test.js
@@ -0,0 +1,141 @@
+$(document).ready(function() {
+  //doc ready
+
+  var topics = [
+    "anger",
+    "fear",
+    "sad",
+    "disgust",
+    "surprise",
+    "anticipation",
+    "trust",
+    "joy"
+  ];
+
+  function addButton() {
+    $("#topics").empty();
+    for (i = 0; i < topics.length; i++) {
+      var newButton = $("<button>");
+      newButton.addClass("btn btn-primary");
+      newButton.attr("data-name", topics[i]);
+      newButton.text(topics[i]);
+      $("#topics").append(newButton);
+    }
+  }
+
+  function addCardColumns() {
+    for (i = 0; i < topics.length; i++) {
+      var images = $("<div>");
+      images.addClass("images card-columns");
+      images.attr("data-name", topics[i]);
+      $("#images").append(images);
+    }
+  }
+
+  addButton();
+  addCardColumns();
+
+  $("#add-button").on("click", function() {
+    event.preventDefault();
+    var topic = $("#add-topic")
+      .val()
+      .trim();
+
+    topics.push(topic);
+    addButton();
+    addCardColumns();
+  });
+
+  $(document).on("click", ".btn", function getGifs() {
+    var url = "https://api.giphy.com/v1/gifs/search";
+    var key = "api_key=TetrpMjBeTZDHJDhshu2qZwVTVoobDCD";
+    var q = $(this).attr("data-name");
+    var limit = "limit=10";
+    var offset = "offset=0";
+    var rating = "rating=PG-13";
+    var lang = "lang=en"; //use a 2-letter ISO 639-1 language code
+    var random_id; //an ID/proxy for specific user
+    var queryURL =
+      url +
+      "?" +
+      key +
+      "&" +
+      "q=" +
+      q +
+      "&" +
+      limit +
+      "&" +
+      offset +
+      "&" +
+      rating +
+      "&" +
+      lang;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      JSON.stringify(response);
+      console.log(response);
+      console.log(q);
+      for (i = 0; i < response.data.length; i++) {
+        console.log(
+          "ID: " +
+            response.data[i].id +
+            ", " +
+            "TITLE: " +
+            response.data[i].title +
+            ", " +
+            "LINK: " +
+            response.data[i].images.fixed_width_still.url
+        );
+
+        var card = $("<div>");
+        var img = $("<img>");
+        var p = $("<p>");
+        var info = $("<div>");
+        var title = $("<h5>");
+        var icon = $("<i>");
+
+        img.addClass("gif card-img-top");
+        img.attr("data-id", response.data[i].id);
+        img.attr("alt", response.data[i].title);
+        img.attr("src", response.data[i].images.fixed_width_still.url);
+        img.attr("data-state", "still");
+        img.attr("data-animate", response.data[i].images.fixed_width.url);
+        img.attr("data-still", response.data[i].images.fixed_width_still.url);
+        icon.addClass("icon far fa-heart");
+        card.addClass("card h-300");
+        info.addClass("card-body");
+        title.addClass("card-title");
+        title.text(response.data[i].title);
+        p.addClass("card-text");
+        p.text("Rating: " + response.data[i].rating);
+
+        $(images).append(card);
+        $(card).append(img);
+        $(card).append(icon);
+        $(card).append(info);
+        $(info).append(title);
+        $(info).append(p);
+      } //for loop close
+    }); //then closing tag
+  }); //getGifs closing tag
+
+  $(document).on("click", ".gif", function toggleState() {
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      var animateVal = $(this).attr("data-animate");
+      $(this).attr("src", animateVal);
+      $(this).attr("data-state", "animate");
+    } else if (state === "animate") {
+      var stillVal = $(this).attr("data-still");
+      $(this).attr("src", stillVal);
+      $(this).attr("data-state", "still");
+    }
+  }); //toggleState closing tag
+
+  addButton();
+  addCardColumns();
+}); //doc ready closing tag
